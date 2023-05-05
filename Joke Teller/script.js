17e12ff6dd8a26da80f87d9e91029461d27001ed
MilanS3333 <110888010+MilanS3333@users.noreply.github.com>
2023-05-05 13:03:01 +0530

    js

---
+++ Joke Teller/script.js
@@ -0,0 +1,49 @@
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
+
+// Disable/Enable Button
+function toggleButton() {
+    button.disabled = !button.disabled;
+}
+
+
+// Passing Joke to VoiceRSS API
+function tellMe(joke) {
+    VoiceRSS.speech({
+        key: putYourOwnKeyHere,
+        src: joke,
+        hl: 'en-us',
+        v: 'Linda',
+        r: 0,
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+}
+
+// Get Jokes from Joke API
+async function getJokes() {
+    let joke = '';
+    const apiUrl = 'https://v2.jokeapi.dev/joke/Any';
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`
+        }
+        else {
+            joke = data.joke;
+        }
+        // Trxt-to-speech
+        tellMe(joke);
+        // Disable Button
+        toggleButton();
+    } catch (error) {
+        alert(error)
+    }
+}
+
+
+// Event Listeners
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
